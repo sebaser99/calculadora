0b24a1363431a2598f217bc4ebbadce79652f0ed
Sebastian Rodriguez <sebaser99@gmail.com>
2022-05-27 13:16:50 -0500

    modificamos el estilo del hover en las teclas

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ function App() {
           <div className='calculadora__resultado'>4</div>
         </div>
         <div className='calculadora__teclado'>
-          <button class='calculadora__tecla calculadora__tecla--gray'>AC</button>
-          <button class='calculadora__tecla calculadora__tecla--gray'>+ / -</button>
-          <button class='calculadora__tecla calculadora__tecla--gray'>%</button>
-          <button class='calculadora__tecla calculadora__tecla--yellow'>÷</button>
+          <button class='calculadora__tecla calculadora__tecla--vine'>AC</button>
+          <button class='calculadora__tecla calculadora__tecla--vine'>+ / -</button>
+          <button class='calculadora__tecla calculadora__tecla--vine'>%</button>
+          <button class='calculadora__tecla calculadora__tecla--red'>÷</button>
           <button class='calculadora__tecla'>7</button>
           <button class='calculadora__tecla'>8</button>
           <button class='calculadora__tecla'>9</button>
-          <button class='calculadora__tecla calculadora__tecla--yellow'>X</button>
+          <button class='calculadora__tecla calculadora__tecla--red'>X</button>
           <button class='calculadora__tecla'>4</button>
           <button class='calculadora__tecla'>5</button>
           <button class='calculadora__tecla'>6</button>
-          <button class='calculadora__tecla calculadora__tecla--yellow'>-</button>
+          <button class='calculadora__tecla calculadora__tecla--red'>-</button>
           <button class='calculadora__tecla'>1</button>
           <button class='calculadora__tecla'>2</button>
           <button class='calculadora__tecla'>3</button>
-          <button class='calculadora__tecla calculadora__tecla--yellow'>+</button>
+          <button class='calculadora__tecla calculadora__tecla--red'>+</button>
           <button class='calculadora__tecla'>0</button>
           <button class='calculadora__tecla'>,</button>
-          <button class='calculadora__tecla calculadora__tecla--span calculadora__tecla--yellow'>=</button>
+          <button class='calculadora__tecla calculadora__tecla--span calculadora__tecla--red'>=</button>
         </div>
       </div>
 
